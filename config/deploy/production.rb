e5ed9409cf47e9f0b0ac83f0d9b6df21aafadaba
songjiayang <songjiayang@weidaxue.me>
2015-01-12 10:20:53 +0800

    change deploy server host

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 # set :stage, :production
 
-server 'zuiux.com', user: 'deploy', roles: %w{web app db}
+server '218.244.128.99', user: 'deploy', roles: %w{web app db}
 set :deploy_to, '/var/www/zuiux'

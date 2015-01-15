5be8230309182c9a032783f54d6c5efe48f83cb4
songjiayang <songjiayang@weidaxue.me>
2015-01-15 18:03:18 +0800

    add qiniu host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = 'http://7u2ko7.com1.z0.glb.clouddn.com'
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -78,6 +78,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'http://zuiux.com' }
-  config.action_mailer.asset_host = 'http://zuiux.com'
+  config.action_mailer.asset_host = 'http://7u2ko7.com1.z0.glb.clouddn.com'
 
 end

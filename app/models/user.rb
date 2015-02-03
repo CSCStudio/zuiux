fea306dae8fc4e9601ed61213832aa58b08ecbd8
songjiayang <songjiayang@weidaxue.me>
2015-02-03 10:24:55 +0800

    rollback

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :lockable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :lockable
 
   has_many :ratings
   has_many :products
